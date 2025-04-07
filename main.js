9f82a612671135fb1579dd3e41b3a3d1847ac120
Vijay Singh <vijaysingh3225@gmail.com>
2025-04-07 16:42:17 -0400

    UI looks better and updated some artifact images

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ const sampleArtifacts = [
     new Artifact("MoonStone", 20, 0, 0, "./images/Artifact-3.png"),
     new StrengthStone("StrengthStone", 20, 0, 0, "./images/Artifact-4.png"),
     new Artifact("VoidReaver", 20, 0, 0, "./images/VoidArtifact2.png"),
+    new Artifact("VoidReaver", 20, 0, 0, "./images/Artifact-5.png"),
+    new Artifact("VoidReaver", 20, 0, 0, "./images/Artifact-6.png"),
 ];
 
 // Tooltip
@@ -254,7 +256,8 @@ function getAdjacentPositions() {
 function addRandomCellButton() {
     const toolbar = document.getElementById("tool-bar");
     const button = document.createElement("button");
-    button.textContent = "Add Random Cell";
+    button.id = "level-button";
+    button.textContent = "Level Up";
     button.onclick = () => {
         const adjacentPositions = getAdjacentPositions();
         if (adjacentPositions.length > 0) {
